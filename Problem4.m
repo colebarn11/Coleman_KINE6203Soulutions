446b8ad5e940d098e5a8b23df951955cdad17d8b
colebarn11 <barnacascelc16@students.ecu.edu>
2021-10-19 19:25:48 -0400

    Revert "Delete Problem4.m"
    This reverts commit 46214384e8ffe62573267f744db55f70a1304111.

---
+++ Problem4.m
@@ -0,0 +1,7 @@
+function [cash] = Problem4(quarters, dimes, nickels, pennies)
+%UNTITLED4 Summary of this function goes here
+%   Detailed explanation goes here
+cash = (quarters * .25) + (dimes * .1) + (nickels * .05) + (pennies * .01)
+
+end
+
